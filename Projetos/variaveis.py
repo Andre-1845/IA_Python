28728ec9597f2c4e86961d96463e127e0a61e8b3
Andre Andrino <andrebaumgratz@gmail.com>
2024-08-08 07:16:57 -0300

    Projetos P755

---
+++ Projetos/variaveis.py
@@ -0,0 +1,9 @@
+num1 = 15
+num2 = 1.98
+cidade = "Brasília"
+
+print("Numero INT : " + str(num1))
+print("Numero FLOAT : " + str(num2))
+print("String : " + cidade)
+print(num1 * num2)
+print(num1 * int(num2))
